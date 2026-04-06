022a8baec723f7017edcde633c75b7b3619712af
Harsh Anvekar <108102421+anvekarHarsh-03@users.noreply.github.com>
2026-04-06 11:22:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,9 +12,15 @@ leads = pd.read_csv("dataset/leads_unclean_25000.csv")
 funnel = pd.read_csv("dataset/funnel_unclean_25000.csv")
 cost = pd.read_csv("dataset/marketing_cost_unclean.csv")
 
-st.write("Leads shape BEFORE cleaning:", leads.shape)
-st.write("Funnel shape BEFORE cleaning:", funnel.shape)
-st.write("Cost shape BEFORE cleaning:", cost.shape)
+st.write("Before date parsing:", leads.shape)
+
+leads["Date"] = pd.to_datetime(leads["Date"], errors="coerce", dayfirst=True)
+
+st.write("Null dates after parsing:", leads["Date"].isna().sum())
+
+leads = leads.dropna(subset=["Date"])
+
+st.write("After date cleaning:", leads.shape)
 
 # =========================
 # CLEAN LEADS DATA
